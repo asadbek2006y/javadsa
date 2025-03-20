bb4d3d2e77fd6c50adf456c6e303148ab57c1e52
Asadbek Otabekov <otabekovasadbek6@gmail.com>
2025-03-21 02:49:45 +0900

    Data Structure bugun 22 ta excersize ishladim va 22 masalaga o'zgartirish kiritdim

--- exc22.java
+++ exc22.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class exc22 {
     public static void main(String[] args) {
         
-        
+        Scanner sc = new Scanner(System.in);
+
+        long binaryNumber, decimalNumber = 0, j = 1, remainder;
+
+        System.out.println("Input binary number : ");
+        binaryNumber = sc.nextLong();
+
+        while (binaryNumber !=0) {
+            remainder = binaryNumber % 10;
+            decimalNumber = decimalNumber + remainder * j;
+            j = j * 2;
+            binaryNumber = binaryNumber / 10;
+        }
+
+        System.out.println("Decimal number : " + decimalNumber);
     }
 }
